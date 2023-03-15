a8f0f78fbd3f41d1a3a2e6bdde9ec821d24b6992
Marcus Ayers <marcayers27@gmail.com>
2023-03-15 13:03:45 -0400

    changed create post to function component

--- app/javascript/src/create_post.jsx
+++ app/javascript/src/create_post.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { handleErrors } from '@utils/fetchHelper';
 
-class CreatePost extends React.Component {
-  state = {
+const CreatePost = ({ userId, subredditId }) => {
+  const [state, setState] = useState({
     isOpen: false,
     title: '',
     body: '',
-  };
+  });
 
-  toggleDropdown = () => {
-    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-    if (!this.state.isOpen) { // PUTS THE CURSOR IN THE TITLE FIELD
+  const toggleDropdown = () => {
+    setState(prevState => ({ ...prevState, isOpen: !prevState.isOpen }));
+    if (!state.isOpen) {
       setTimeout(() => {
         document.getElementById('title').focus();
       }, 0);
     }
   };
 
-  handleChange = (event) => {
+  const handleChange = event => {
     const { name, value } = event.target;
-    this.setState({
+    setState({
+      ...state,
       [name]: value,
     });
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const { title, body } = this.state;
-    const { userId, subredditId } = this.props;
+    const { title, body } = state;
     const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
     fetch(`/api/subreddits/${subredditId}/posts`, {
@@ -44,43 +44,42 @@ class CreatePost extends React.Component {
       }),
     })
       .then(handleErrors)
-      .then((data) => {
+      .then(data => {
         console.log(data);
-        this.setState({
+        setState({
+          ...state,
           isOpen: false,
           title: '',
           body: '',
         });
         window.location.reload();
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
         window.alert("You need to be logged in to create a post");
-
       });
   };
 
-  render() {
-    const { isOpen, title, body } = this.state;
+  const { isOpen, title, body } = state;
 
-    return (
-      <div className="container">
-        <div className="row">
+  return (
+    <div className="container">
+      <div className="row">
           <div className="col create-post-button mt-3 mb-3 d-flex align-items-center">
             <img src="https://www.redditstatic.com/avatars/avatar_default_02_94E044.png" alt="Italian Trulli" className="profile-pic"></img>
-            <input type="text" className="create-post mx-auto" onClick={this.toggleDropdown} placeholder="Create Post" />
+            <input type="text" className="create-post mx-auto" onClick={toggleDropdown} placeholder="Create Post" />
 
             {isOpen && (
               <div className="dropdown-menu">
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={handleSubmit}>
                   <div className="form-group">
                     <label className='post-title-big' htmlFor="title">Title</label>
-                    <input type="text" className="form-control text-white" id="title" name="title" value={title} onChange={this.handleChange} required />
+                    <input type="text" className="form-control text-white" id="title" name="title" value={title} onChange={handleChange} required />
                   </div>
 
                   <div className="form-group">
                     <label className='post-title-big' htmlFor="body">Body</label>
-                    <textarea className="form-control text-white" id="body" name="body" rows="3" value={body} onChange={this.handleChange} required></textarea>
+                    <textarea className="form-control text-white" id="body" name="body" rows="3" value={body} onChange={handleChange} required></textarea>
                   </div>
 
                   <button type="submit" className="btn btn-primary">
@@ -91,9 +90,8 @@ class CreatePost extends React.Component {
             )}
           </div>
         </div>
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CreatePost;
