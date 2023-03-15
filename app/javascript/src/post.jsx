7c0a78e30c39c33e5d3693cc68135747f2f9a46f
Marcus Ayers <marcayers27@gmail.com>
2023-03-15 13:40:47 -0400

    convert post to function component

--- app/javascript/src/post.jsx
+++ app/javascript/src/post.jsx
@@ -1,149 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Layout from '@src/layout';
 import { handleErrors, safeCredentials } from '@utils/fetchHelper';
 
-class Post extends React.Component {
+const Post = (props) => {
+  const [post, setPost] = useState([]);
+  const [subreddit, setSubreddit] = useState([]);
+  const [comment, setComment] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState([]);
+  const [authenticated, setAuthenticated] = useState(false);
 
-
-  state = {
-    post: [],
-    subreddit: [],
-    comment: [],
-    loading: true,
-    username: [],
-    authenticated: false,
-
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch('/api/authenticated')
       .then(handleErrors)
-      .then(data => {
-        // console.log(data)
-        this.setState({
-          username: data.username,
-          authenticated: data.authenticated,
-        })
+      .then((data) => {
+        setUsername(data.username);
+        setAuthenticated(data.authenticated);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("XYXYXYXYXYXYXYXYXYYXYXYXYXYY " + error);
       });
 
-    fetch(`/api/subreddits/${this.props.subreddit_id}`)
+    fetch(`/api/subreddits/${props.subreddit_id}`)
       .then(handleErrors)
-      .then(data => {
-        // console.log("hello1")
-        this.setState({
-          subreddit: data.subreddit,
-          loading: false,
-        })
+      .then((data) => {
+        setSubreddit(data.subreddit);
+        setLoading(false);
       });
 
-    fetch(`/api/subreddits/${this.props.subreddit_id}/posts/${this.props.post_id}`)
+    fetch(`/api/subreddits/${props.subreddit_id}/posts/${props.post_id}`)
       .then(handleErrors)
-      .then(data => {
+      .then((data) => {
+        setPost(data.post);
+        setLoading(false);
+      });
 
-        this.setState({
-          post: data.post,
-          loading: false,
-        })
-      })
-      fetch(`/api/subreddits/${this.props.subreddit_id}/posts`)
+    fetch(`/api/subreddits/${props.subreddit_id}/posts`)
       .then(handleErrors)
-      .then(data => {
-        this.setState({
-          posts: data.posts,
-          loading: false,
-          
-        })
-      })
+      .then((data) => {
+        setLoading(false);
+      });
 
-      fetch(`/api/subreddits/${this.props.subreddit_id}/posts/${this.props.post_id}/comments`)
+    fetch(`/api/subreddits/${props.subreddit_id}/posts/${props.post_id}/comments`)
       .then(handleErrors)
-      .then(data => {
-        this.setState({
-          posts: data.posts,
-          loading: false,
-          
-        })
-      })
+      .then((data) => {
+        setLoading(false);
+      });
+  }, [props.subreddit_id, props.post_id]);
+
+  const removePost = (e) => {
+    fetch(`/api/subreddits/${props.subreddit_id}/posts/${props.post_id}`, safeCredentials({
+      method: 'DELETE',
+  }))
+  .then(response => {
+    // console.log(response)
+      if (!response.ok) {
+          throw Error(response.statusText);
+      }
+      return response.json();
+  })
+  .then(data => {
+      console.log(data); 
+      if (data.success) {
+        window.location.href = '/';
+      }
+  })
+  .catch(error => {
+    
+      console.error(error);
+      setState({
+          error: 'Sorry, there was a problem deleting the post. Please try again later.'
+      });
+  });
+  };
+
+  if (loading) {
+    return <p>loading...</p>;
   }
 
+  const description = subreddit?.description;
+  const name = subreddit?.name;
+  const id = post?.id;
+  const title = post?.title;
+  const body = comment?.body;
+  const date = new Date(post?.created_at);
+  const dateToString = date?.toLocaleString();
+  const postUser = post?.user ? post.user.username : "";
+  const username2 = post?.user?.username;
 
-
-   
-  removePost = e => {
-
-    e.preventDefault();
-    fetch(`/api/subreddits/${this.props.subreddit_id}/posts/${this.props.post_id}`, safeCredentials({
-        method: 'DELETE',
-    }))
-    .then(response => {
-      // console.log(response)
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data); 
-        if (data.success) {
-          window.location.href = '/';
-        }
-    })
-    .catch(error => {
-      
-        console.error(error);
-        this.setState({
-            error: 'Sorry, there was a problem deleting the post. Please try again later.'
-        });
-    });
-  }
-
-  
-  render () {
-    
-    const { post, subreddit, loading, comment, username } = this.state;
-    if (loading) {
-      return <p>loading...</p>;
-    };
-    const {
-      description,
-      name,
-    } = subreddit 
-    
-    const {
-      id,
-      title,
-    } = post
-
-   const {
-     body,
-   } = comment
-
-   const date = new Date(post.created_at)
-   const dateToString = date.toLocaleString();
-   const postUser = post.user ? post.user.username : "";
-   const username2 = post.user?.username 
-
-    return (
-      <Layout>
-        <div className="container">
+  return (
+    <Layout>
+       <div className="container">
           <div className="row">
             <div className=" col-7 mr-5 mb-3 post-background">
               <div className="">
               <div className="mb-3">
-              <a href={`/user/${post.user?.id}`}>
-              <p className='post-info'>Posted by u/{post.user?.username} - {dateToString}</p>
+              <a href={`/user/${post?.user?.id}`}>
+              <p className='post-info'>Posted by u/{post?.user?.username} - {dateToString}</p>
               </a>
               {username === postUser &&
               <a href={'/subreddit/1'} >
-                  <button type="button" className="btn btn-danger delete-post-button " onClick={this.removePost} >delete</button>
+                  <button type="button" className="btn btn-danger delete-post-button " onClick={removePost} >delete</button>
               </a>
                 }
                 <h3 className="mb-0 post-title-big">{title}</h3>
-                <p className="mb-0 post-description"><small><b>{post.body}</b></small></p>
+                <p className="mb-0 post-description"><small><b>{post?.body}</b></small></p>
               </div>
               <hr />
               </div>
@@ -159,22 +121,16 @@ class Post extends React.Component {
             </div>
           </div>
         </div>
-      </Layout>
-    )
-  }
-}
-
-
+    </Layout>
+  );
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('params');
   const data = JSON.parse(node.getAttribute('data-params'));
-  // console.log(data)
-  // console.log("The subreddit ID is " + data.subreddit_id)
-  // console.log("The post ID is " + data.post_id)
+
   ReactDOM.render(
     <Post subreddit_id={data.subreddit_id} post_id={data.post_id} />,
     document.body.appendChild(document.createElement('div')),
-  )
-})
-
+  );
+});
